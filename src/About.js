5e6198f809142bf98a6b2272eba8d6eb67df2c97
mnatsakanyan <artur.mnatsakanyan@gmail.com>
2018-06-20 17:29:43 +0400

    first message

---
+++ src/About.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const About = () => {
+  return <p>this is About component</p>;
+};
+export default About;
---
+++ src/Home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = () => {
+  return <p>this is Home component</p>;
+};
+
+export default Home;
